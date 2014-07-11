e2fca017fc3887e511ba3d4ea30e2e8e8c2a68c4
Sa Vo <support@mobi-cart.com>
2014-07-11 16:06:32 +0700

    update DataParser and DataHandler

--- Source/trunk/Family/Model/DataParser/DataParser.m
+++ Source/trunk/Family/Model/DataParser/DataParser.m
@@ -1,13 +1,28 @@
 //
-//  DataParser.m
+//  Utilities.m
 //  Family
 //
-//  Created by Admin on 7/7/14.
+//  Created by Admin on 7/1/14.
 //  Copyright (c) 2014 Admin. All rights reserved.
 //
 
 #import "DataParser.h"
+#import "Member.h"
+#import "Activity.h"
+#import "Promise.h"
+#import "FMDatabase.h"
 
 @implementation DataParser
 
++ (Member*)allocMemberWithResults:(FMResultSet*)results{
+    return nil;
+}
+
++ (Activity*)allocAcitiviyWithResults:(FMResultSet*)results{
+    return nil;
+}
+
++ (Promise*)allocPromiseWithResults:(FMResultSet*)results{
+    return nil;
+}
 @end
